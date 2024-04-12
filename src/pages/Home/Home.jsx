a998bdcf3b68c5bd1701274defc392a117bfdcb0
PabloTei <pteijeirolopez@gmail.com>
2024-04-12 18:40:27 +0200

    fix layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     <>
       <section className="hero">
         <div className="section">
-          <h1>¡Hola, soy Pablo!</h1>
+          <h1>¡Hola, soy Pablo 👨‍💻!</h1>
           <ul>
             <li>
               <a href={links.linkedin} target="_blank">
